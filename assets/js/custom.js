be439549906bc1171eca0b082fba5fcbf3ce5323
nnhshu <nnhshu97@gmail.com>
2019-11-02 16:47:27 +0700

    update font and add new file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,8 +12,8 @@
             navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
             autoplay: true,
             autoplayTimeout: 5000,
-            animateIn: 'lightSpeedIn',
-            animateOut: 'lightSpeedOut',
+            animateIn: 'fadeInUp',
+            animateOut: 'fadeInDown',
             smartSpeed: 250
         });
 
